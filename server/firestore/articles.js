38f0a10f98079b1537229ae8a217df125e5705bb
MarciaAR <marciaarambulo@gmail.com>
2021-07-12 15:49:53 -0400

    Adds user to view all articles

--- server/firestore/articles.js
+++ server/firestore/articles.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
-const firebase = require('../db/db');
-const Article = require('../db/models/Article');
+const firebase = require("../db/db");
+const Article = require("../db/models/Article");
 const db = firebase.firestore();
-const scraperObj = require('../puppeteer');
+const scraperObj = require("../puppeteer");
 
 const getAllArticles = async (req, res, next) => {
   try {
     const articles = await db
-      .collection('users')
-      .doc('t2D8ahpahoxhxE8xvOG4')
-      .collection('Articles');
+      .collection("users")
+      .doc("HCP12AR3aNctzasRsyWtPWJeHJL2")
+      .collection("Articles");
     const data = await articles.get();
     const articlesArray = [];
     if (data.empty) {
-      res.status(404).send('No articles found');
+      res.status(404).send("No articles found");
     } else {
       data.forEach((doc) => {
         const article = new Article(
@@ -38,13 +38,13 @@ const getSingleArticle = async (req, res, next) => {
   try {
     // const id = req.params.id;
     const article = await db
-      .collection('users')
-      .doc('t2D8ahpahoxhxE8xvOG4')
-      .collection('Articles')
-      .doc('c5HnOtaD45D8RmipTh9e');
+      .collection("users")
+      .doc("t2D8ahpahoxhxE8xvOG4")
+      .collection("Articles")
+      .doc("c5HnOtaD45D8RmipTh9e");
     const data = await article.get();
     if (!data.exists) {
-      res.status(404).send('Article not found');
+      res.status(404).send("Article not found");
     } else {
       res.send(data.data());
     }
@@ -56,23 +56,23 @@ const getSingleArticle = async (req, res, next) => {
 const addArticle = async (req, res, next) => {
   try {
     const url =
-      'https://medium.com/creatures/3-things-you-probably-didnt-know-about-cats-cb643c57b382';
+      "https://medium.com/creatures/3-things-you-probably-didnt-know-about-cats-cb643c57b382";
     let resource;
-    if (url.includes('medium')) {
+    if (url.includes("medium")) {
       resource = scraperObj.medium;
-    } else if (url.includes('nytimes')) {
+    } else if (url.includes("nytimes")) {
       resource = scraperObj.nytimes;
-    } else if (url.includes('wikipedia')) {
+    } else if (url.includes("wikipedia")) {
       resource = scraperObj.wikipedia;
     }
     const data = await resource(url);
     await db
-      .collection('users')
-      .doc('t2D8ahpahoxhxE8xvOG4')
-      .collection('Articles')
+      .collection("users")
+      .doc("t2D8ahpahoxhxE8xvOG4")
+      .collection("Articles")
       .doc()
       .set(data);
-    res.send('Record saved successfuly');
+    res.send("Record saved successfuly");
   } catch (error) {
     res.status(400).send(error.message);
   }
@@ -82,12 +82,12 @@ const deleteArticle = async (req, res, next) => {
   try {
     // const id = req.params.id;
     await db
-      .collection('users')
-      .doc('t2D8ahpahoxhxE8xvOG4')
-      .collection('Articles')
-      .doc('U3mDGvfOyjiyqccyzzqa')
+      .collection("users")
+      .doc("t2D8ahpahoxhxE8xvOG4")
+      .collection("Articles")
+      .doc("U3mDGvfOyjiyqccyzzqa")
       .delete();
-    res.send('Record deleted successfuly');
+    res.send("Record deleted successfuly");
   } catch (error) {
     res.status(400).send(error.message);
   }
