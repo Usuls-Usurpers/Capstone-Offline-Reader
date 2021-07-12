aee6a87187dd00ad053759787739e69c00f10963
Inna Lynn <innalynn@Innas-MacBook-Air.local>
2021-07-12 12:32:14 -0400

    adding axios call to post a new article into the chrome extension popup.js file

--- client/chrome-extension/popup.js
+++ client/chrome-extension/popup.js
@@ -1,9 +1,14 @@
 let sendUrl = document.getElementById("send-url");
+import axios from "axios"
 
-// When the button is clicked it should console log the URL
-//eventually this will send the url to the webscraper
+const dispatchUrl = async (url) => {
+    return await axios.post('http://localhost:8080/api/article', url) 
+} 
+// When the button is clicked it should send the url to the webscraper
 sendUrl.addEventListener("click", async () => {
   console.log('click!')
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true})
   console.log(`current tab url is ${tab.url}`)
+  const status = await dispatchUrl(tab.url)
+  console.log (status)
 })
