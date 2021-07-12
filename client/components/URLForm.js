df9225e95fda15f813b248c519869c39701794ed
Inna Lynn <innalynn@Innas-MacBook-Air.local>
2021-07-12 12:50:59 -0400

    added thunk creator and action/creator for adding article by URL

---
+++ client/components/URLForm.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+
+class URLForm extends Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            URL:''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    handleSubmit(event){
+        event.preventDefault()
+    }
+    render() {
+        const {URL} =  this.state
+        return (
+            <div className="add-div" >
+                <h3>Save an Article</h3>
+                <form className="add-form" onSubmit={this.handleSubmit}>
+                    <lable htmlFor="URL">URL</lable>
+                    <input name="URL" onChange={this.handleChange} value={URL} />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    addArticleByURL: (student) => dispatch(addArticleByURL(URL, history))
+})
+export default connect(null, mapDispatchToProps)(URLForm)
+
--- client/store/articles.js
+++ client/store/articles.js
@@ -2,37 +2,59 @@ import axios from "axios";
 
 //ACTION TYPES
 
-const SET_ARTICLES = 'SET_ARTICLES';
+const SET_ARTICLES = 'SET_ARTICLES'
+const ADD_ACTICLE = 'ADD_ACTICLE'
 
 //ACTION CREATORS
 
 export const setArticles = (articles) => {
   return {
     type: SET_ARTICLES,
-    articles,
-  };
-};
+    articles
+  }
+}
+
+export const addArticle = (article) => {
+  return {
+    type: ADD_ACTICLE,
+    article
+  }
+} 
 
 //THUNK CREATORS
 
 export const fetchArticles = () => {
   return async (dispatch) => {
     try {
-      const { data } = await axios.get("/api/articles");
-      console.log("data", data);
-      dispatch(setArticles(data));
+      const { data } = await axios.get("/api/articles")
+      console.log("data", data)
+      dispatch(setArticles(data))
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
   };
 };
+
+export const addArticleByURL = (url) =>{
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.post("/api/article", url)
+      console.log("data", data)
+      dispatch(addArticle(data))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
 
 // STUDENT SUB_REDUCER
 export default function articlesReducer(articles = [], action) {
   switch (action.type) {
     case SET_ARTICLES:
-      return action.articles;
+      return action.articles
+    case ADD_ACTICLE:
+      return [...articles, action.article]
     default:
-      return articles;
+      return articles
   }
 }
