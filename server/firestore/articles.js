4391d0dd5d875027361ea97c824a068de4f20bda
Regina Kim <kimregina2757@gmail.com>
2021-07-13 16:16:41 -0400

    (feat): functional delete article button

--- server/firestore/articles.js
+++ server/firestore/articles.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 
-const firebase = require("../db/db");
-const Article = require("../db/models/Article");
+const firebase = require('../db/db');
+const Article = require('../db/models/Article');
 const db = firebase.firestore();
-const scraperObj = require("../puppeteer");
+const scraperObj = require('../puppeteer');
 
 const getAllArticles = async (req, res, next) => {
   try {
-    const userId = req.query.uid
+    const userId = req.query.uid;
     const articles = await db
-      .collection("users")
+      .collection('users')
       .doc(`${userId}`)
-      .collection("Articles");
+      .collection('Articles');
     const data = await articles.get();
     const articlesArray = [];
     if (data.empty) {
-      res.status(404).send("No articles found");
+      res.status(404).send('No articles found');
     } else {
       data.forEach((doc) => {
         const article = new Article(
@@ -30,24 +30,25 @@ const getAllArticles = async (req, res, next) => {
       });
       res.send(articlesArray);
       // res.send('Hello World!');
-  }} catch (error) {
+    }
+  } catch (error) {
     res.status(400).send(error.message);
   }
-}
+};
 
 const getSingleArticle = async (req, res, next) => {
   try {
     const id = req.params.articleId;
-    console.log("ID: ", id);
-    console.log("req.params: ", req.params);
+    console.log('ID: ', id);
+    console.log('req.params: ', req.params);
     const article = await db
-      .collection("users")
-      .doc("KmRkLorXqpRLug4pbK1Fs8g15fx1")
-      .collection("Articles")
+      .collection('users')
+      .doc('KmRkLorXqpRLug4pbK1Fs8g15fx1')
+      .collection('Articles')
       .doc(id);
     const data = await article.get();
     if (!data.exists) {
-      res.status(404).send("Article not found");
+      res.status(404).send('Article not found');
     } else {
       res.send(data.data());
     }
@@ -58,21 +59,21 @@ const getSingleArticle = async (req, res, next) => {
 
 const addArticle = async (req, res, next) => {
   try {
-    console.log('req in addArticle', req)
+    // console.log('req in addArticle', req)
     const { url, userId } = req.body;
     let resource;
-    if (url.includes("medium")) {
+    if (url.includes('medium')) {
       resource = scraperObj.medium;
-    } else if (url.includes("nytimes")) {
+    } else if (url.includes('nytimes')) {
       resource = scraperObj.nytimes;
-    } else if (url.includes("wikipedia")) {
+    } else if (url.includes('wikipedia')) {
       resource = scraperObj.wikipedia;
     }
     const data = await resource(url);
     await db
-      .collection("users")
+      .collection('users')
       .doc(`${userId}`)
-      .collection("Articles")
+      .collection('Articles')
       .doc()
       .set(data);
     res.send(data);
@@ -85,15 +86,14 @@ const addArticle = async (req, res, next) => {
 const deleteArticle = async (req, res, next) => {
   try {
     // const id = req.params.id;
+    const { userId, articleId } = req.body;
     await db
-      .collection("users")
-      .doc("t2D8ahpahoxhxE8xvOG4")
-      .collection("Articles")
-      .doc("U3mDGvfOyjiyqccyzzqa")
+      .collection('users')
+      .doc(userId)
+      .collection('Articles')
+      .doc(articleId)
       .delete();
-
     // res.send('Record deleted successfuly');
-
   } catch (error) {
     res.status(400).send(error.message);
   }
