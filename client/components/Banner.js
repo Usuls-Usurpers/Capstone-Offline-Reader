c0c0be91ea4af881b40ca44844ee5f4eee9e60c4
MarciaAR <marciaarambulo@gmail.com>
2021-07-15 22:54:01 -0400

    Adds logo to home  page only

--- client/components/Banner.js
+++ client/components/Banner.js
@@ -1,5 +1,23 @@
 import React, { Component } from "react";
 
+{
+  /* <NavLink to="/">
+            <img
+              src="https://i.imgur.com/uFNnZAt.png"
+              alt="polly pocket logo"
+              style={stylesheet.logo}
+            />
+          </NavLink>
+          <div>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/signup">Sign Up</NavLink>
+          </div>
+        </div>
+      )}
+    </nav>
+  </AppBar> */
+}
+
 class Banner extends Component {
   render() {
     return (
--- client/components/Home.js
+++ client/components/Home.js
@@ -1,36 +1,13 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import URLForm from "./URLForm";
-
-// const name = (username) => {
-//   let email = username.split("@")[0];
-//   let firstLetter = username[0].toUpperCase();
-//   return firstLetter.concat(email.slice(1));
-// };
 
 class Home extends Component {
   render() {
-    const { name } = this.props;
     return (
       <div id="welcome">
-        {this.props.isLoggedIn ? (
-          <div>
-            <h3>Welcome, {name}!</h3>
-            <URLForm history={this.props.history} />
-          </div>
-        ) : (
-          <h3>Welcome to Cache-22!</h3>
-        )}
+        <h3>Welcome to Cache-22!</h3>
+        <img src="https://i.imgur.com/LH3S0X4.png" alt="cache-22 logo" />
       </div>
     );
   }
 }
-const mapState = (state) => {
-  return {
-    // username: state.auth.email,
-    isLoggedIn: !!state.auth.uid,
-    name: state.auth.displayName
-  };
-};
-
-export default connect(mapState)(Home);
+export default Home;
