d0983164f0768db955a56ee739ccf89e0e009ef2
MarciaAR <marciaarambulo@gmail.com>
2021-07-20 17:58:13 -0400

    Modifies SW

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,17 +1,18 @@
 "use strict";
 
-const proofCacheFiles = [
-  ".",
-  "index.html",
-  "style.css",
-  "client/components/AllArticles.js",
-];
+const proofCacheFiles = ["index.html", "style.css"];
 
-function cacheAssets() {
-  caches.open("cache-v1").then(function (cache) {
-    return cache.addAll(proofCacheFiles);
-  });
-}
+// const cacheAssets = async () => {
+//   caches.open("cache-v1").then(function (cache) {
+//     return cache.addAll(proofCacheFiles);
+//   });
+// }
+
+const cacheAssets = async () => {
+  const cache = await caches.open("cache-v1");
+  console.log("From SW: Caching app shell and content");
+  await cache.addAll(proofCacheFiles);
+};
 
 //Listen to install and activate events
 self.addEventListener("install", (event) => {
@@ -22,6 +23,18 @@ self.addEventListener("install", (event) => {
 //Good place to clear up older versions of cache
 self.addEventListener("activate", (event) => {
   console.log("From SW: Active Event", event);
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      Promise.all(
+        keyList.map((key) => {
+          if (key === cacheName) {
+            return;
+          }
+          caches.delete(key);
+        })
+      );
+    })()
+  );
 });
 
 self.addEventListener("fetch", (event) => {
@@ -32,3 +45,16 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
+//  (async () => {
+//    const r = await caches.match(e.request);
+//    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+//    if (r) {
+//      return r;
+//    }
+//    const response = await fetch(e.request);
+//    const cache = await caches.open(cacheName);
+//    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//    cache.put(e.request, response.clone());
+//    return response;
+//  })();
